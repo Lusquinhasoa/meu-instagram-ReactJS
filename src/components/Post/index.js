e97ac946053985fe5c96b5c256047b858b237517
Lucas de Oliveira Alencar <lucasalencaar7@gmail.com>
2022-05-06 21:09:19 -0300

    Ajuste da Header e Footer do post
    foi ajustado a posição dos botões de comentário e likes deixando similar ao original do site, além de outros ajustes no campo do post

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,12 +1,18 @@
 import './style.css'
 import {FiMoreHorizontal} from 'react-icons/fi'
+import {IoMdHeartEmpty} from 'react-icons/io'
+import {BsChat,BsEmojiSmile} from 'react-icons/bs'
+import {FiSend} from 'react-icons/fi'
+import {BsBookmark} from 'react-icons/bs'
+import { IconContext } from 'react-icons'
+
 export function Post(){
     return(
         <>
-        <header>
+        <header className='header-post'>
 
             <div className="infos-post">
-            <img src="https://staticg.sportskeeda.com/editor/2019/09/03ead-15688918737420-800.jpg"/>
+            <img className='img-header-post' src="https://staticg.sportskeeda.com/editor/2019/09/03ead-15688918737420-800.jpg"/>
 
             <p>John Cena</p>
             </div>
@@ -18,9 +24,48 @@ export function Post(){
         </div>
 
         <div className='footer-post'>
+            <IconContext.Provider value={{size:'30px'}}>
             <section className='engagement-post'>
+                <div className='icons1'>
+                <div className='icon'><IoMdHeartEmpty/></div>
+                <div className='icon'><BsChat/></div>
+                <div className='icon'><FiSend/></div>
+                </div>
+
+                <div className='icon'><BsBookmark/></div>
 
             </section>
+            </IconContext.Provider>
+
+        <section className='likes'>
+            <span>200.000 curtidas </span>
+        </section>
+
+        <div className='legend'>
+            <p>
+                <strong>John Cena</strong>  
+                  If you don´t watch my series, I am gonna ki*k your as* mother**
+            </p>
+
+            <div className='clock'>
+                <time>HÁ 3 HORAS</time>
+            </div>
+
+            <div className='comment'>
+                <div className='fake-comment'>
+                <IconContext.Provider value={{size: '25px'}}>
+                    <div className='icon'>
+                    <BsEmojiSmile />
+                    </div>
+                </IconContext.Provider>
+
+                <input placeholder='Adicione um comentário...'/>
+                </div>
+
+                <button>Publicar</button>
+        </div>
+
+        </div>
         </div>
         </>
     )
