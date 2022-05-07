5415d370d6b085f1fac6e1771383c0dfc550a830
Lucas de Oliveira Alencar <lucasalencaar7@gmail.com>
2022-05-06 21:39:43 -0300

    Adição dos stories de amigos
    Adicionei os stories de amigos próximos para deixar o layout mais parecido com o do site.

--- src/components/Story/index.js
+++ src/components/Story/index.js
@@ -13,58 +13,58 @@ export function Story(){
 
             <div className='user-elements'>
                 <div>
-                    <img className='image-user-st' src='https://d1fdloi71mui9q.cloudfront.net/O6FisBcARCuXUbKtDnGS_bN6v5pka897OFv5u' />
+                    <img className='image-user-st' src='https://d1fdloi71mui9q.cloudfront.net/AD4otGADSH60ey1Gog7E_cMadGs3VqCELRuLg' />
                 </div>
 
-                <span>Guilherme Briggs</span>
+                <span>Shawn Mendes</span>
             </div>
 
             <div className='user-elements'>
                 <div>
-                    <img className='image-user-st' src='https://d1fdloi71mui9q.cloudfront.net/O6FisBcARCuXUbKtDnGS_bN6v5pka897OFv5u' />
+                    <img className='image-user-st' src='https://akns-images.eonline.com/eol_images/Entire_Site/2021717/rs_1024x1024-210817153300-1024-dylan-obrien-blonde.jpg?fit=around%7C1024:1024&output-quality=90&crop=1024:1024;center,top' />
                 </div>
 
-                <span>Guilherme Briggs</span>
+                <span>imdylan</span>
             </div>
 
             <div className='user-elements'>
                 <div>
-                    <img className='image-user-st' src='https://d1fdloi71mui9q.cloudfront.net/O6FisBcARCuXUbKtDnGS_bN6v5pka897OFv5u' />
+                    <img className='image-user-st' src='https://www.aceshowbiz.com/images/photo/the_rock.jpg' />
                 </div>
 
-                <span>Guilherme Briggs</span>
+                <span>The Rock</span>
             </div>
 
             <div className='user-elements'>
                 <div>
-                    <img className='image-user-st' src='https://d1fdloi71mui9q.cloudfront.net/O6FisBcARCuXUbKtDnGS_bN6v5pka897OFv5u' />
+                    <img className='image-user-st' src='https://i.pinimg.com/474x/a4/f5/7e/a4f57e3170b1a76a0534a7d940e125d4.jpg' />
                 </div>
 
-                <span>Guilherme Briggs</span>
+                <span>Camila Cabello</span>
             </div>
 
             <div className='user-elements'>
                 <div>
-                    <img className='image-user-st' src='https://d1fdloi71mui9q.cloudfront.net/O6FisBcARCuXUbKtDnGS_bN6v5pka897OFv5u' />
+                    <img className='image-user-st' src='https://scontent.fsod8-1.fna.fbcdn.net/v/t1.6435-9/101847092_3001723589921921_8239884239001092096_n.png?_nc_cat=1&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=OOvoDTyzZe8AX_FHqvZ&_nc_ht=scontent.fsod8-1.fna&oh=00_AT8aP6pMVnDBmqI-rFbo34uFWa3hT65wYyp0rFl5v_KJ0w&oe=629CC12C' />
                 </div>
 
-                <span>Guilherme Briggs</span>
+                <span>Instituto PROA</span>
             </div>
 
             <div className='user-elements'>
                 <div>
-                    <img className='image-user-st' src='https://d1fdloi71mui9q.cloudfront.net/O6FisBcARCuXUbKtDnGS_bN6v5pka897OFv5u' />
+                    <img className='image-user-st' src='https://pbs.twimg.com/profile_images/1211774325596020741/ktychaOA_400x400.jpg' />
                 </div>
 
-                <span>Guilherme Briggs</span>
+                <span>BRKsEDU</span>
             </div>
 
             <div className='user-elements'>
                 <div>
-                    <img className='image-user-st' src='https://d1fdloi71mui9q.cloudfront.net/O6FisBcARCuXUbKtDnGS_bN6v5pka897OFv5u' />
+                    <img className='image-user-st' src='https://yt3.ggpht.com/DIEqRepL0JEro5q3en3lLUyI_vqmot1yghANEaDjANJULzP-2BaewW0XSQX-mzbAgmp4EuODjTA=s900-c-k-c0x00ffffff-no-rj' />
                 </div>
 
-                <span>Guilherme Briggs</span>
+                <span>Olivia Rodrigo</span>
             </div>
             
         </div>
